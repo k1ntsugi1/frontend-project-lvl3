32e117a5e04c84c20b1751df2195f45dfca876b5
Bulat Masalimov <bmasalimov5@gmail.com>
2022-04-16 21:32:06 +0200

    first step

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 [![Maintainability](https://api.codeclimate.com/v1/badges/376681579f70502eda39/maintainability)](https://codeclimate.com/github/k1ntsugi1/frontend-project-lvl3/maintainability)
 ### Test Coverage Badge:
 <a href="https://codeclimate.com/github/k1ntsugi1/frontend-project-lvl3/test_coverage"><img src="https://api.codeclimate.com/v1/badges/376681579f70502eda39/test_coverage" /></a>
+### Vercel:
+https://frontend-project-lvl3-k1ntsugi1.vercel.app/
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  module: {
+    rules: [
+      { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+      },
+    ],
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+  ],
+};

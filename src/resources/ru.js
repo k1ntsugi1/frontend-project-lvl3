9d11a042727a1da10a5ca8d98a5c6e2dcf11a8af
Bulat Masalimov <bmasalimov5@gmail.com>
2022-04-17 05:29:41 +0200

    add resources

---
+++ src/resources/ru.js
@@ -0,0 +1,10 @@
+export default {
+  ru: {
+    translation: {
+      isNotURL: 'Ссылка должна быть валидным URL',
+      urlInAddedResources: 'RSS уже существует',
+      isNotRssURL: 'Ресурс не содержит валидный RSS',
+      isValid: 'RSS успешно загружен',
+    },
+  },
+};

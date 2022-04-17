708f36e381a883c3f2d1b14d042fe9f0b98660f8
Bulat Masalimov <bmasalimov5@gmail.com>
2022-04-17 12:42:11 +0200

    edit webpack-config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: {
+    main: './src/index.js',
+  },
   module: {
     rules: [
       { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
@@ -14,12 +17,13 @@ module.exports = {
     ],
   },
   output: {
-    filename: 'main.js',
+    filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './index.html',
     }),
+    new CleanWebpackPlugin(),
   ],
 };

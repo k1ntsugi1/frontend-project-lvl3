2af8cfeceb914d8a12bd884c5bfc4f9d5ecd63e3
Bulat Masalimov <bmasalimov5@gmail.com>
2022-04-17 06:27:20 +0200

    edit

--- src/init.js
+++ src/init.js
@@ -4,7 +4,7 @@ import languages from './resources/languages.js';
 import app from './app.js';
 
 const runApp = () => {
-  new Promise((resolve) => {
+  const promise = new Promise((resolve) => {
     const i18Instance = i18next.createInstance();
     i18Instance.init({
       lng: 'ru',
@@ -12,28 +12,28 @@ const runApp = () => {
       resources: languages.ru,
     });
     resolve(i18Instance);
+  });
+  promise.then((i18nInst) => {
+    console.log(i18nInst);
+    const state = {
+      i18n: i18nInst,
+      resultOfValidation: {
+        message: null,
+        isValid: { status: false },
+      },
+      addedResources: [],
+    };
+    state.shemes = {
+      urlValidationScheme: yup.object({
+        rssUrl: yup.string(state.i18n.t('isNotURL'))
+          .url(state.i18n.t('isNotURL'))
+          .required(state.i18n.t('isNotURL'))
+          .trim(state.i18n.t('isNotURL'))
+          .matches(/.rss$/, { message: state.i18n.t('isNotRssURL') }),
+      }),
+    };
+    return state;
   })
-    .then((i18nInst) => {
-      console.log(i18nInst);
-      const state = {
-        i18n: i18nInst,
-        resultOfValidation: {
-          message: null,
-          isValid: { status: false },
-        },
-        addedResources: [],
-      };
-      state.shemes = {
-        urlValidationScheme: yup.object({
-          rssUrl: yup.string(state.i18n.t('isNotURL'))
-            .url(state.i18n.t('isNotURL'))
-            .required(state.i18n.t('isNotURL'))
-            .trim(state.i18n.t('isNotURL'))
-            .matches(/.rss$/, { message: state.i18n.t('isNotRssURL') }),
-        }),
-      };
-      return state;
-    })
     .then((state) => {
       app(state);
     })

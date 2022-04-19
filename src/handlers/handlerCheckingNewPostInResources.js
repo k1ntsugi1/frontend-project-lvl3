7d36fa199b622b6b19d89a7d3380ad76994b710b
Bulat Masalimov <bmasalimov5@gmail.com>
2022-04-19 02:11:55 +0200

    fifth step

--- src/handlers/handlerCheckingNewPostInResources.js
+++ src/handlers/handlerCheckingNewPostInResources.js
@@ -1,41 +1,38 @@
 import axios from 'axios';
 import parserRSS from '../parsers/parserRss';
-// eslint-disable-next-line import/no-self-import
-import handlerSetTimeout from './handlerCheckingNewPostInResources.js';
+// eslint-disable-next-line import/no-cycle
+import handlerSetTimeout from './handlerSetTimeout.js';
 
 const handlerCheckingNewPostInResources = (watcherLoadingRSSContent) => {
   const { topics: oldTopics, resources } = watcherLoadingRSSContent;
   const proxy = 'https://allorigins.hexlet.app/get?';
-  const promises = resources.map((resource) => {
-    return axios.get(`${proxy}disableCache=true&url=${encodeURIComponent(resource.value)}/`)
-    .then((response) => parserRSS(response, resource.id))
-  });
+  const promises = resources.map((resource) => axios.get(`${proxy}disableCache=true&url=${encodeURIComponent(resource.value)}/`)
+    .then((response) => parserRSS(response, resource.id)));
 
   Promise.all(promises)
-  .then((parsedResources) => {
-    parsedResources.forEach((parsedRss) => {
-    const { topics } = parsedRss;
-    const [{ currentId }] = topics;
-    const oldTobicsWithCurrentId = oldTopics.filter(({ id }) => currentId === id)
-      .map(({ title }) => title);
-    const newTopics = topics.filter(({ title }) => !oldTobicsWithCurrentId.includes(title));
-    if (newTopics.length === 0) return;
-    newTopics.forEach((newTopic) => watcherLoadingRSSContent.topics.push(newTopic))})
+    .then((parsedResources) => {
+      console.log(parsedResources, 'parsedRSS');
+      parsedResources.forEach((parsedRss) => {
+        const { topics } = parsedRss;
+        const { feed } = parsedRss;
+        const { id: currentId } = feed;
+        console.log(parsedRss, 'parsedRSS');
+        // eslint-disable-next-line max-len
+        const oldTobicsWithCurrentId = oldTopics.filter(({ id }) => currentId === id).map(({ title }) => title);
+        const newTopics = topics.filter(({ title }) => !oldTobicsWithCurrentId.includes(title));
+
+        if (newTopics.length === 0) return;
+        newTopics.forEach((newTopic) => watcherLoadingRSSContent.topics.push(newTopic));
+      });
+    })
     .then(() => {
-    watcherLoadingRSSContent.addingCounter += 1;
-    handlerSetTimeout(watcherLoadingRSSContent, true);
-  }).catch((e) => {
-    console.log(e);
-    watcherLoadingRSSContent.addingCounter = 0;
-    handlerSetTimeout(watcherLoadingRSSContent, false);
-  });
+      watcherLoadingRSSContent.addingCounter += 1;
+      handlerSetTimeout(watcherLoadingRSSContent, true);
+    }).catch((e) => {
+      console.log(e);
+      watcherLoadingRSSContent.addingCounter = 0;
+      handlerSetTimeout(watcherLoadingRSSContent, false);
+    });
 };
 
 export default handlerCheckingNewPostInResources;
-/*
-        watcherLoadingRSSContent.addingCounter += 1;
-        handlerSetTimeout(watcherLoadingRSSContent, true);
-
-        watcherLoadingRSSContent.addingCounter = 0;
-        handlerSetTimeout(watcherLoadingRSSContent, false);
-*/

908e1384a46dc5997bb6d7d40b47571e6e59ef58
Bulat Masalimov <bmasalimov5@gmail.com>
2022-04-22 15:29:48 +0200

    12

--- src/handlers/getUniqId.js
+++ src/handlers/getUniqId.js
@@ -1,3 +1,3 @@
-const uuid =()=>([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g,c=>(c^crypto.getRandomValues(new Uint8Array(1))[0]&15 >> c/4).toString(16));
+const uuid = () => 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c, r) => (с == 'x' ? (r = Math.random() * 16 | 0) : (r & 0x3 | 0x8)).toString(16));
 
 export default uuid;

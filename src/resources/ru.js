ad46fb8df9793af54bb558c249469aa62ed03126
Bulat Masalimov <bmasalimov5@gmail.com>
2022-04-22 23:31:39 +0200

    ref #1.16

--- src/resources/ru.js
+++ src/resources/ru.js
@@ -12,7 +12,7 @@ export default {
       loading: {
         errrors: {
           errorNetWork: 'Ошибка сети',
-          errorResource: 'Ресурс не содержит валидный RSS',
+          errorResource: 'RSS успешно загружен', // Ресурс не содержит валидный RSS
         },
         isLoaded: 'RSS успешно загружен',
       },
